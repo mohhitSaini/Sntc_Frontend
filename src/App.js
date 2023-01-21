5b7dc16557dee97b7338994ee66a16c77c579b8a
Amam <varshneyaman3105@gmail.com>
2023-01-21 19:53:08 +0530

    Dark_Mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar'
-
+import React, { useState } from 'react';
 function App() {
+  const [mode, setmode] = useState('light');
+  const [state, setstate] = useState('Enable Dark Mode');
+  const toggleMode = () => {
+    if (mode === 'light' ) {
+      setmode('dark');
+      setstate('Enable Light Mode')
+      document.body.style.backgroundColor = 'black';
+    }
+    else {
+      setmode('light');
+      setstate('Enable Dark Mode')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
-    <Navbar/>
+    <Navbar mode={mode} state={ state} toggleMode = {toggleMode} />
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 import logo from './images/Xpecto.png';
-export default function navbar() {
+export default function navbar(props) {
     return (
         <>
             <nav id="navbar">
@@ -31,7 +31,15 @@ export default function navbar() {
                     <li className="item"><a href="#">Events</a></li>
                     <li className="item"><a href="#">Work Report</a></li>
                     <li className="item"><a href="#">Gallery</a></li>
+                    <li className = "toggle">
+                        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.state}</label>
+                        </div>
+                    </li>
+                    
                 </ul>
+
             </nav>
            
         </>
