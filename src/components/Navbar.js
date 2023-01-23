87c68536b5d79a5b504dbeb3b4eb2281404f0a23
Amam <varshneyaman3105@gmail.com>
2023-01-23 13:26:43 +0530

    Hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,45 +3,82 @@ import './style.css';
 import logo from './images/Xpecto.png';
 export default function navbar(props) {
     return (
-        <>
-            <nav id="navbar">
-                <div id="logo">
-                    <img src={logo} alt="" />
-                </div>
-                <ul>
-                    <li className="item"><a href="#">Home</a></li>
-                    <li className="item"><a href="#">About</a></li>
-                    <li className="item"><a href="#">Inter IIT</a></li>
-                    <li className="item"><a href="#">GSOC</a></li>
-                    <li className="item"><a href="#">Xpecto</a></li>
-                    <li className="item"><a href="#">Services</a></li>
-                    <li className="item"><a href="#">Projects</a></li>
-                    <div id="clubs">
 
-                    <li className="item1"><a href="#"><div className="dropdown">
-                        Clubs
-                        <div className="dropdown-content">
-                            <a href="#">Pc</a>
-                            <a href="#">Yantrik</a>
-                            <a href="#">Gdsc</a>
-                        </div>
-                    </div></a></li>
-                    </div>
-                    <li className="item"><a href="#">Team</a></li>
-                    <li className="item"><a href="#">Events</a></li>
-                    <li className="item"><a href="#">Work Report</a></li>
-                    <li className="item"><a href="#">Gallery</a></li>
-                    <li className = "toggle">
-                        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
-                            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
-                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.state}</label>
-                        </div>
-                    </li>
+
+        <div className = "mynav">
+          <nav className="navbar navbar-expand-lg">
+              <div className="container-fluid">
+                  <img src={logo} alt="" />
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                          <li className="nav-item">
+                              <a className="nav-link" aria-current="page" href="#">Home</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">About</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Team</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Events</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Work Report</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Gallery</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Services</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Projects</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">GSOC</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Inter IIT</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-link" href="#">Xpecto</a>
+                          </li>
+                          <li className="nav-item dropdown">
+                              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                  Clubs
+                              </a>
+                              <ul className="dropdown-menu">
+                                  <li><a className="dropdown-item" href="#">KP</a></li>
+                                  <li><a className="dropdown-item" href="#">Yaantrik</a></li>
+                                  <li><a className="dropdown-item" href="#">GDSC</a></li>
+                              </ul>
+                            </li>
+                            <li className="nav-item toggler">
+                                <div className="check-toggle">
+
+                                    <li className="toggle">
+                                        <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'}`}>
+                                            <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault" />
+                                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.state}</label>
+                                        </div>
+                                    </li>
+                                </div>
+                            </li>
+                           
+                          
+                      </ul>
+                      
+                  </div>
+              </div>
+          </nav>
+    </div>
+       
                     
-                </ul>
-
-            </nav>
-           
-        </>
+                    
+                
     )
 }
